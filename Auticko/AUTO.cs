4f6d03cc96c33b4aaab7ec0a4fa288edc9eedf98
FrostoBeasto <tomaskment.cv@gmail.com>
2025-03-25 10:53:51 +0100

    Přidejte soubory projektu.

---
+++ Auticko/AUTO.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auticko
+{
+    internal class AUTO
+    {
+        private double palivo = 55;
+
+        public void natankuj()
+        {
+            Console.WriteLine($"Natankoval si {55-palivo} paliva");
+        }
+
+        public void stav_paliva()
+        {
+            Console.WriteLine($"Aktuální stav paliva {palivo}");
+        }
+
+        public void jed()
+        {
+            int spotreba = 6;
+            if (palivo == 0)
+            {
+                Console.WriteLine("Není palivo bro");
+            }
+            else
+            {
+                Console.WriteLine("Kolik km chces ujet?");
+                double km = Convert.ToInt32(Console.ReadLine());
+                double pouzite_palivo = (km / 100) * spotreba;
+                pouzite_palivo = Math.Round(pouzite_palivo, 2);
+                palivo -= pouzite_palivo;
+                Console.WriteLine($"Ujel si {km} a spotřeboval si {pouzite_palivo} litru paliva");
+            }
+        }
+    }
+}
---
+++ Auticko/Program.cs
@@ -0,0 +1,25 @@
+﻿using Auticko;
+
+bool cyklus = true;
+AUTO auto = new AUTO();
+while (cyklus == true)
+{
+    Console.WriteLine("Co chceš dělat se svím autíčkem?");
+    Console.WriteLine("1.Natankovat\n2.Podívat se na stav paliva\n3.Jed\n4.Konec");
+    int volba = Convert.ToInt32(Console.ReadLine());
+    switch(volba)
+    {
+        case 1:
+            auto.natankuj();
+            break;
+        case 2:
+            auto.stav_paliva();
+            break;
+        case 3:
+            auto.jed();
+            break;
+        case 4:
+            cyklus = false;
+            return;
+    }
+}
